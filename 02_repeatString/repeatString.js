d815ff9e665ab120d3b2acb3cd80cdebb2b83c1e
wolfie-ssh <tilakg0p1605@gmail.com>
2024-10-23 11:29:50 +0530

    complete "02_repeatString"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+ if (num < 0) return "Error";
+ let word = "";
+ for (let i = 0; i < num; i++) {
+    word += string ;
+ }
+ return word;
 };
 
 // Do not edit below this line
