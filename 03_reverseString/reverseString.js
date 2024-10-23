1f425dcece475bf084042624572c7fc8fd55406a
wolfie-ssh <tilakg0p1605@gmail.com>
2024-10-23 12:01:07 +0530

    complete "03_reverseString"

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(word) {
+  let len = word.length;
+  let reverseWord = "";
+  for (let i = len-1; i >= 0; i--) {
+    reverseWord += word[i];
+   }
+   return reverseWord;
+ };
 
 // Do not edit below this line
 module.exports = reverseString;
