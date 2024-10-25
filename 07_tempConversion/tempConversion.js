2dd3155dd5e5357c42d932a20432bd49fff74acf
wolfie-ssh <tilakg0p1605@gmail.com>
2024-10-26 00:01:33 +0530

    complete "07_tempConversion"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputFahrenheit) {
+let c = (inputFahrenheit - 32) * (5/9);
+
+return (Math.round(c*10)/10);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputCelcius) {
+let f = (inputCelcius * (9/5)) + 32;
+return (Math.round(f*10)/10);
 };
 
 // Do not edit below this line
