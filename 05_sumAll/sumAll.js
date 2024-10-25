624ae1ce6ff82ce06fcb15b42866f5596c1c8514
wolfie-ssh <tilakg0p1605@gmail.com>
2024-10-25 19:58:25 +0530

    complete "05_sumAll"

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum)  {
+    if (
+        typeof firstNum !== 'number' ||
+        typeof lastNum !== 'number' ||
+        firstNum < 0 ||
+        lastNum < 0 ||
+        !Number.isInteger(firstNum) ||
+        !Number.isInteger(lastNum)
+    ) {
+        return "ERROR";
+    }
+    if (firstNum > lastNum) {
+        let tempOne = firstNum;
+        let tempTwo = lastNum;
+        firstNum = tempTwo;
+        lastNum = tempOne;
+    }
 
-};
-
+    let temp = 0;
+    for (let i = firstNum; i <= lastNum; i++) {
+        temp = temp + i;
+    }
+    return temp;
+}
 // Do not edit below this line
 module.exports = sumAll;
